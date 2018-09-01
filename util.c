12e2deeab74ac9471e0434e277a2e8271b53d171
Akira Moroo <retrage01@gmail.com>
2018-09-01 13:32:53 +0000

    Add missing util.c
    Add calc_offset function

---
+++ util.c
@@ -0,0 +1,13 @@
+#include "minc.h"
+
+int calc_offset(Map *env) {
+  int offset = 0;
+  Vector *keys = map_keys(env);
+  for (int i = 0; i < vector_size(keys); i++) {
+    Type *ty = map_get(env, vector_get(keys, i));
+    offset += ty->offset;
+  }
+
+  return offset;
+}
+
